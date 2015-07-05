8f2b0f7bb64b02c1aa9f83ec3654327dba10d7cd
Eric Masse <ericmas001@hotmail.com>
2015-07-05 13:06:49 -0400

    Changing Application Name

--- C#/BluffinMuffin.Client/Properties/AssemblyInfo.cs
+++ C#/BluffinMuffin.Client/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BluffinMuffin.Client")]
+[assembly: AssemblyTitle("Client BluffinMufin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BluffinMuffin.Client")]
+[assembly: AssemblyProduct("Client BluffinMufin")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
